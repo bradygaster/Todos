d2d762f5ce01224af723db5ec11cb50211a4f642
bradygaster <bradyg@microsoft.com>
2021-10-15 00:56:04 -0700

    committing

--- Todos.API/TodoDbContext.cs
+++ Todos.API/TodoDbContext.cs
@@ -16,7 +16,7 @@ namespace Todos.API
                 new Todo { Id = 20, Title = "Create a new todo item." },
                 new Todo { Id = 30, Title = "Mark the new todo item complete." },
                 new Todo { Id = 40, Title = "Delete the todo item you created." },
-                new Todo { Id = 50, Title = "Right-click-publish the API project to Azure App Service (Windows). During the publishing phase, you'll need to create an Application Insights resource and an Azure SQL Database using Visual Studio Connected Services. When you create the SQL DB, make sure you change the connection string to be `TodoDb` and *not* the default `ConnectionStrings:TodoDb` setting. Once you've published the code to App Service and it has opened in the browser, mark this todo item complete."},
+                new Todo { Id = 50, Title = "Right-click-publish the API project to Azure App Service (Windows). During the publishing phase, you'll need to create an Application Insights resource and an Azure SQL Database using Visual Studio Connected Services. When you create the SQL DB, make sure you change the connection string to be `TodoDb`, not the default `ConnectionStrings:TodoDb` setting. Once you've published the code to App Service and it has opened in the browser, mark this todo item complete."},
                 new Todo { Id = 60, Title = "Add a new todo in the live site, mark it complete, and then, delete it. Then mark this todo complete."}
             );
 #endif 
