aa580c2adc8f9a088d26ac8855de2c1059bccecc
bradygaster <bradyg@microsoft.com>
2021-10-15 18:54:25 -0700

    devcontainer

--- setup.ps1
+++ setup.ps1
@@ -5,21 +5,16 @@ param (
     $location='westus'
 )
 
-if (Test-Path .\api.zip) {
-    Remove-Item .\api.zip
-}
-
-try {
-    dotnet tool install --global Zipper --version 1.0.1
-} catch {
-    Write-Host "Environment already has Zipper installed. Yay!" -ForegroundColor Yellow
-}
+az group create -l westus -n $resourceBaseName
+az deployment group create --resource-group $resourceBaseName --template-file resources.bicep --parameters resourceBaseName=$resourceBaseName --parameters sqlUsername=$sqlServerDbUsername --parameters sqlPassword=$sqlServerDbPwd 
 
 dotnet build Todos.API\Todos.API.csproj
 dotnet publish Todos.API\Todos.API.csproj -o publish\api
-zipper compress -i publish\api -o api.zip
 
-az group create -l westus -n $resourceBaseName
-az deployment group create --resource-group $resourceBaseName --template-file resources.bicep --parameters resourceBaseName=$resourceBaseName --parameters sqlUsername=$sqlServerDbUsername --parameters sqlPassword=$sqlServerDbPwd 
+if (Test-Path .\api.zip) {
+    Remove-Item .\api.zip
+}
+Get-ChildItem -Path .\publish\api\ | Compress-Archive -DestinationPath api.zip
+
 az webapp deploy -n "$($resourceBaseName)-api" -g $resourceBaseName --src-path api.zip
 az webapp browse -n "$($resourceBaseName)-api" -g $resourceBaseName
