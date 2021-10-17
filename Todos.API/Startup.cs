50b030fa5c435bdb90e93f5e38290772370dec77
bradygaster <bradyg@microsoft.com>
2021-10-17 10:18:46 -0700

    now the app will use sqlite for debugging, so it'll work on f5 in codespaces as well as in vs.

--- Todos.API/Startup.cs
+++ Todos.API/Startup.cs
@@ -19,7 +19,11 @@ namespace Todos.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+#if DEBUG
+            services.AddDbContext<TodoDbContext>(optionsBuilder => optionsBuilder.UseSqlite($"Data Source=todos.db"));
+#else
             services.AddDbContext<TodoDbContext>(optionsBuilder => optionsBuilder.UseSqlServer(Configuration.GetConnectionString("TodoDb")));
+#endif
             services.AddControllers();
             services.AddSwaggerGen();
             services.AddApplicationMapName(Configuration);
